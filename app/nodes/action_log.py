9c0ca81c748807d5e079b90c35addd067c67507e
Flux <danny@turnr.io>
2026-05-19 00:04:42 +0200

    Fix IMP-F33: add entry-level logger.info to action_log

--- app/nodes/action_log.py
+++ app/nodes/action_log.py
@@ -10,6 +10,7 @@ LABEL = "Log"
 
 def run(config, inp, context, logger, creds=None, **kwargs):
     """Log a message to the execution logs."""
+    logger.info("action_log: starting")
     msg = _render(config.get('message', ''), context, creds)
     logger.info("LOG: %s", msg)
     return {'logged': msg}
